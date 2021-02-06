72f076f4f0e1ce8afba46d1791f96394576b1c22
Lior Hirsch <liorhirsch@gmail.com>
2021-02-06 19:58:39 +0200

    implement k threads to BFS_search

--- BFS_search.py
+++ BFS_search.py
@@ -1,21 +1,53 @@
+import timeit
+from datetime import datetime
 from queue import Queue
 from puzzle import Puzzle
+import time
+import numpy as np
 
 
-def breadth_first_search(initial_state):
+
+
+def breadth_first_search(initial_state, thread_number=1):
     start_node = Puzzle(initial_state, None, None, 0)
     if start_node.goal_test():
         return start_node.find_solution()
     q = Queue()
     q.put(start_node)
-    explored=[]
-    while not(q.empty()):
-        node=q.get()
+
+    q_temp = Queue()
+    explored = []
+    i = 0
+    total_steps = 0
+    total_time = np.float64(0)
+    threads_list_times = []
+
+
+    while not (q.empty()):
+        t0 = time.clock()
+        i += 1
+        node = q.get()
         explored.append(node.state)
-        children=node.generate_child()
+        children = node.generate_child()
         for child in children:
             if child.state not in explored:
-                if child.goal_test():
-                    return child.find_solution()
-                q.put(child)
+                q_temp.put(child)
+
+        # t1 = np.datetime64()
+
+        t1 = time.clock()
+        diff = t1 - t0
+        threads_list_times.append(diff)
+
+        if i == thread_number or q.empty():
+            total_time += max(threads_list_times)
+            threads_list_times = []
+            i = 0
+            total_steps += 1
+
+            while not q_temp.empty():
+                curr_node = q_temp.get()
+                if curr_node.goal_test():
+                    return total_time, curr_node.find_solution(), total_steps
+                q.put(curr_node)
     return
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from BFS_search import breadth_first_search
 from Astar_search import Astar_search
 from RBFS_search import recursive_best_first_search
 from puzzle import Puzzle
-
+import numpy as np
 
 state=[[1, 3, 4,
         8, 6, 2,
@@ -17,32 +17,31 @@ state=[[1, 3, 4,
         4, 6, 3,
         0, 7, 5]]
 
-for i in range(0,3):
-    Puzzle.num_of_instances=0
-    t0=time()
-    bfs=breadth_first_search(state[i])
-    t1=time()-t0
-    print('BFS:', bfs)
-    print('space:',Puzzle.num_of_instances)
-    print('time:',t1)
-    print()
-
-    Puzzle.num_of_instances = 0
-    t0 = time()
-    astar = Astar_search(state[i])
-    t1 = time() - t0
-    print('A*:',astar)
-    print('space:', Puzzle.num_of_instances)
-    print('time:', t1)
-    print()
-
-    Puzzle.num_of_instances = 0
-    t0 = time()
-    RBFS = recursive_best_first_search(state[i])
-    t1 = time() - t0
-    print('RBFS:',RBFS)
-    print('space:', Puzzle.num_of_instances)
-    print('time:', t1)
-    print()
+for i in range(0,1):
+    for k in range(1,50):
+        all_times = []
+        for j in range(1000):
+            Puzzle.num_of_instances = 0
+            total_time, solution, total_steps = breadth_first_search(state[i], k)
+            all_times.append(total_time)
+        print(f"K = {k} | space : {Puzzle.num_of_instances} | total time : {np.average(all_times)} | total steps : {total_steps}")
+    #
+    # Puzzle.num_of_instances = 0
+    # t0 = time()
+    # astar = Astar_search(state[i])
+    # t1 = time() - t0
+    # print('A*:',astar)
+    # print('space:', Puzzle.num_of_instances)
+    # print('time:', t1)
+    # print()
+    #
+    # Puzzle.num_of_instances = 0
+    # t0 = time()
+    # RBFS = recursive_best_first_search(state[i])
+    # t1 = time() - t0
+    # print('RBFS:',RBFS)
+    # print('space:', Puzzle.num_of_instances)
+    # print('time:', t1)
+    # print()
 
     print('------------------------------------------')
